33931d4ffbec8043cde3f47303804ae5a54e2dd8
Misho64 <61743234+Misho64@users.noreply.github.com>
2021-04-02 00:57:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,317 @@
+//✠▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ☢❦۞❦☢ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬✠//BY: POULAZ (Angry Black)
+
+const Discord = require("discord.js");
+const myid = ['720708440707825755'];//هنا بتحط الايدى بتاعك بتاع الديسكورد
+const developers = ['720708440707825755'];// هنا بتحط الايدى بتاع الحسابات اللى هيتحكم فى البوتات
+const prefix = ['%'];
+const cmd = require("node-cmd")
+const client1 = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
+const client16 = new Discord.Client();
+const client17 = new Discord.Client();
+const client18 = new Discord.Client();
+const client19 = new Discord.Client();
+const client20 = new Discord.Client();
+const client21 = new Discord.Client();
+const client22 = new Discord.Client();
+const client23 = new Discord.Client();
+const client24 = new Discord.Client();
+const client25 = new Discord.Client();
+const client26 = new Discord.Client();
+
+
+//✠▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ☢❦۞❦☢ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬✠//BY: POULAZ (Angry Black)
+
+client1.login("NzAwOTkyOTYzODk5MDMxNTUy.X4daWA.ghFJHMhnSJ-Q2R18dA8AprxsowM"); 
+client2.login("NzIwNzAzMzU5NjA2MzI1MzMw.X4devw.NMCXrSeUJ4qYTBDNt2RnkVVfQZk"); 
+client3.login("Njk0MjcxNzg3NzcyNTQzMDk3.X4dYAw.KbT-mLQtVVkZ5vpJX-s_xjkgcx0"); 
+client4.login("NzIxMTQxOTc1MDU5MDA1NDUw.X4ddEg.lzxum9XYM4dvAMDQTTfpn3UVI74");
+client5.login("NzIwNzA4NDQwNzA3ODI1NzU1.X4dT_A._qlM9LqBy1j9w6wO7mIGfOuON2s");
+client6.login("NzIwNzA1MzUwNDMyNDU2ODM2.YCgkAg.QEoM6tvztMH_sYgsfK7DJYtV6ss");
+client7.login("NzIxNzA4MjQwMzAzMjkyNDQ3.X4dhHg.5HAc1fLmY4uRlCHKOovm4fOxBg8");
+//__________$$&&____________________________________________
+client8.login();("NzE4MTY1MTkwMTU5NjMwNDA3.YCaGJA.BKarVQm90jfzAvNvKxPWhMJkrVI");//#9337
+client9.login("NzI4OTA4MTE3MzkxNDQxOTQw.YCaIrA.uHifOJmacTl-hASxzq0jycbqk7g");//#4268
+client10.login("NzQ2MzYxNjgzMTY5MTE2MzIw.YC5vAA.R6anY-Tt1BJ0whF_5sQZ4qmOb9g");//#5594
+client11.login("NTkzODQ3NDgzODU2MDYwNDE2.YC5vhw.7Jwow4RfW5zh3fW2b8JP6lwrZ7s");//#3188
+//_________________________&&$$_____________________________
+client12.login();
+client13.login();
+client14.login();
+client15.login();
+client16.login();
+client17.login();
+client18.login();
+client19.login();
+client20.login();
+client21.login();
+client22.login();
+client23.login();
+client24.login();
+client25.login();
+client26.login();
+
+//✠▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ☢❦۞❦☢ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬✠//BY: POULAZ (Angry Black)
+
+client1.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+
+client2.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+
+client3.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+
+client4.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+
+client5.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client6.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+
+client7.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+//____________________$$&&__________________________________
+
+client8.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+
+client9.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+
+client10.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+
+client11.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+
+
+client12.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client13.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client14.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client15.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client16.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client17.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client18.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client19.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client20.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client21.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client22.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client23.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client24.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client25.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+client26.on("messageReactionAdd", async(reaction , user ) => {
+  if(user.bot)return;
+   if(reaction.message.author.id == "396464677032427530"){
+     reaction.message.react(reaction.emoji.name)
+   }
+
+
+})
+
+
+
+
